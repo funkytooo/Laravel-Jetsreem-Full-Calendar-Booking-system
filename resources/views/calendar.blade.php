498db641229bc0e4118c99ac1e453daffe721aec
Nev <neven.shtiliyanov@gmail.com>
2021-02-13 04:03:35 +0000

    Modal

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -3,8 +3,10 @@
 <head>
     <title>Laravel Fullcalender Tutorial Tutorial - ItSolutionStuff.com</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+     integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+      crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.5.1/main.min.css" 
     integrity="sha256-uq9PNlMzB+1h01Ij9cx7zeE2OR2pLAfRw3uUUOOPKdA=" crossorigin="anonymous">
@@ -14,6 +16,9 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
 </head>
 <body>
   
@@ -26,32 +31,63 @@ document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    selectable: false,
+    selectable: true,
     nowIndicator: true,
     headerToolbar: {
       center: '',
       left:'title',
       right: 'dayGridMonth timeGridWeek timeGridDay today prev,next'
     },
-    dateClick: function() {
-        var dateStr = prompt('Enter text');
-       // if (!isNaN(date.valueOf())) { // valid?
-            calendar.addEvent({
-              title: 'dynamic event',
-              start: date,
-              allDay: true
-            });
-          //  alert('Succesful');
-        //  } else {
-         //   alert('');
-   // }
-}
+
+     //When u select some space in the calendar do the following:
+     select: function (day) {
+            //do something when space selected
+            //Show 'add event' modal
+            $('#peno').modal('show');
+        },
+
+        //When u drop an event in the calendar do the following:
+        eventDrop: function (event, delta, revertFunc) {
+            //do something when event is dropped at a new location
+        },
+
+        //When u resize an event in the calendar do the following:
+        eventResize: function (event, delta, revertFunc) {
+            //do something when event is resized
+        },
+
+        eventRender: function(event, element) {
+            $(element).tooltip({title: event.title});             
+        },
+
+        //Activating modal for 'when an event is clicked'
+        dayClick: function () {
+          $('#peno').modal('show');
+            
+        },
+      
 
   });
 
   calendar.render();
 });
   </script>
-  
+<div class="modal" id= peno tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
